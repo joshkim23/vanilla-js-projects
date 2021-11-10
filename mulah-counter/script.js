4162dbc2c9b663725ddf080fc6b658f51c93e4d9
Joshua Kim <joshkim23@gmail.com>
2021-11-09 19:41:38 -0800

    added ability to specify initial dollar amount

--- mulah-counter/script.js
+++ mulah-counter/script.js
@@ -6,16 +6,17 @@ const minEl = document.querySelector('.min');
 const secEl = document.querySelector('.sec');
 const startStopButton = document.querySelector('.go-stop-button');
 const headerEl = document.querySelector('.header');
+const startedWithEl = document.querySelector('.started-with');
 
 // helper tool for measuring time 
 const timeElapsedArray = [hoursEl, minEl, secEl];
 
 // States for the app
 let hourlyRate = 40;
-let totalMulah = 0;
 let buttonIsGo = true;
 let timer; 
 let elapsedTime = 0;
+let startedWith = 0;
 /* 
     The app needs to make the input fields immutable while running
     Toggle between a green go button and red stop button when clicked and track elapsed time and calculate dollar value
@@ -24,7 +25,7 @@ let elapsedTime = 0;
 function start() {
     timeElapsedArray.forEach(el => el.innerHTML = '0');
     hourlyRateEl.value = hourlyRate.toString();
-    totalMulahEl.value = totalMulah.toString();
+    totalMulahEl.value = '';
     headerEl.innerHTML = "Amount Earned so Far"
 }
 start();
@@ -46,12 +47,17 @@ function toggleButton() {
 
 startStopButton.addEventListener('click', () => toggleButton())
 
+totalMulahEl.addEventListener('input', (e) => {
+    startedWith = parseInt(e.target.value);   
+    startedWithEl.innerHTML = e.target.value;
+})
+
 function trackTime() {   
-    timeElapsedArray[0].innerHTML = Math.floor(elapsedTime/3600).toString(); // convert seconds to hours
+    timeElapsedArray[0].innerHTML = Math.floor(elapsedTime / 3600).toString(); // convert seconds to hours
     timeElapsedArray[1].innerHTML = Math.floor(elapsedTime / 60 % 60).toString(); // converts to minutes and rolls over
     timeElapsedArray[2].innerHTML = Math.floor(elapsedTime % 60).toString();
-    
-    totalMulahEl.value = ((hourlyRate/3600*(elapsedTime)).toFixed(2)).toString() // $40/hr * 1 hr/3600 sec * sec
+
+    totalMulahEl.value = (startedWith + (hourlyRate/3600*(elapsedTime))).toFixed(2).toString() // $40/hr * 1 hr/3600 sec * sec
     
     elapsedTime++;
     timer = setTimeout(trackTime, 1000);
